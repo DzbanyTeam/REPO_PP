25ad983dae07ade6efed0d53e37568fe23af362b
Robert Wszelaki <rpwszelaki@gmail.com>
2019-04-23 17:20:10 +0200

    Skrypt inicjalizujący konfigurację intelliJ dla systemu Windows
    Poprzedni sposób nie miał prawa działać.
    Dodano pliki inicjalizacyjne w osobnym folderze oraz skrypt intellij-config-init.bat (na razie tylko dla Windowsa) który skopiuje je w odpowiednie miejsce.
    Po sklonowaniu repo najpierw uruchomcie ten skrypt, wtedy wszystko powinno działać a jednocześnie nie będziemy wersjonować plików konfiguracji.

---
+++ intellij-config-init.bat
@@ -0,0 +1,7 @@
+xcopy /s /i /Y "%~dp0/intellij-config-init-files/.idea" "%~dp0/skisystem/.idea"
+
+xcopy /s /i /Y "%~dp0/intellij-config-init-files/SkiSystemAdmin" "%~dp0/skisystem/SkiSystemAdmin"
+
+xcopy /s /i /Y "%~dp0/intellij-config-init-files/SkiSystemCustomer" "%~dp0/skisystem/SkiSystemCustomer"
+
+xcopy /s /i /Y "%~dp0/intellij-config-init-files/SkiSystemDomain" "%~dp0/skisystem/SkiSystemDomain"
