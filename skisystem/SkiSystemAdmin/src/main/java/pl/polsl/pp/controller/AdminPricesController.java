3d050ad6f8a0d284543b7a4d45b6ee3a276d3511
Przemek <wylezek.przemyslaw@gmail.com>
2019-04-23 18:57:36 +0200

    Stworzenie nowych kontrolerów w panelu admina

---
+++ skisystem/SkiSystemAdmin/src/main/java/pl/polsl/pp/controller/AdminPricesController.java
@@ -0,0 +1,9 @@
+//package pl.polsl.pp.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping("/admin/prices")
+//public class AdminPricesController {
+//}
