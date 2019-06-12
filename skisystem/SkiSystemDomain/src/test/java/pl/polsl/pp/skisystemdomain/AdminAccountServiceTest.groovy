de65576cc4871d62ad21ebd20c4f976d25c49ddc
Marcel <marcel.gasztych@gmail.com>
2019-06-12 15:39:09 +0200

    Finished AdminAccountService tests

--- skisystem/SkiSystemDomain/src/test/java/pl/polsl/pp/skisystemdomain/AdminAccountServiceTest.groovy
+++ skisystem/SkiSystemDomain/src/test/java/pl/polsl/pp/skisystemdomain/AdminAccountServiceTest.groovy
@@ -30,16 +30,17 @@ class AdminAccountServiceTest extends Specification {
         def adminAccount = adminAccountService.getAdminAccountById(0)
 
         then:
+        adminAccount == null
         noExceptionThrown()
     }
     //getAdminAccountByUsername
     def "should return AdminAccount by username"() {
         given:
         def adminAccount = TestDataGenerator.createAdminAccount()
-        def wasSuccessful = adminAccountService.saveAdminAccount(adminAccount)
+        adminAccountService.saveAdminAccount(adminAccount)
 
         when:
-        AdminAccount returnedAdminAccount = adminAccountService.getAdminAccountByUsername(adminAccount.getName())
+        AdminAccount returnedAdminAccount = adminAccountService.getAdminAccountByUsername(adminAccount.getUsername())
 
         then:
         adminAccount.name == returnedAdminAccount.getName()
@@ -70,17 +71,106 @@ class AdminAccountServiceTest extends Specification {
         wasSuccessful
         noExceptionThrown()
     }
-    def "shouldn not add new Admin"() {
+    def "should change Admin Account's Password"() {
         given:
-        def adminAccount = TestDataGenerator.createIncorrectAdminAccount()
+        def adminAccount = TestDataGenerator.createAdminAccount()
 
         when:
-        def wasSuccessful = adminAccountService.saveAdminAccount(adminAccount)
+        adminAccountService.saveAdminAccount(adminAccount)
+        adminAccount.setPassword("NewPassword")
+        adminAccountService.saveAdminAccount(adminAccount)
 
         then:
-        !wasSuccessful
+        adminAccount.getPassword() == adminAccountService.getAdminAccountById(adminAccount.getId()).getPassword()
         noExceptionThrown()
     }
+    def "should not add new Admin"() {
+        expect:
+        !adminAccountService.saveAdminAccount(TestDataGenerator.createAdminAccountWithoutPassword())
+        !adminAccountService.saveAdminAccount(TestDataGenerator.createAdminAccountWithoutUsername())
+    }
+    //deleteAdminAccounts
+    def "should delete Admin Accounts"() {
+        given:
+        def adminAccount = TestDataGenerator.createAdminAccount()
+        def adminAccount1 = TestDataGenerator.createAdminAccount()
 
+        adminAccountService.saveAdminAccount(adminAccount)
+        adminAccountService.saveAdminAccount(adminAccount1)
 
+        List<Long> accountsList = [adminAccount.getId(), adminAccount1.getId()]
+
+        when:
+        adminAccountService.deleteAdminAccounts(accountsList)
+
+        then:
+        adminAccountService.getAdminAccountByUsername(adminAccount.getUsername()) == null
+        adminAccountService.getAdminAccountByUsername(adminAccount1.getUsername()) == null
+        noExceptionThrown()
+    }
+    def "should not delete Admin Accounts"() {
+        expect:
+        adminAccountService.deleteAdminAccounts([-1, 0]) == false
+    }
+
+    //deactivateAdminAccounts
+    def "should deactivate Admin Accounts"() {
+        given:
+        def adminAccount = TestDataGenerator.createAdminAccount()
+        def adminAccount1 = TestDataGenerator.createAdminAccount()
+
+        adminAccountService.saveAdminAccount(adminAccount)
+        adminAccountService.saveAdminAccount(adminAccount1)
+
+        when:
+        adminAccountService.deactivateAdminAccounts([adminAccount.getId(), adminAccount1.getId()])
+
+        then:
+        !adminAccount.getIsActive()
+        !adminAccount1.getIsActive()
+    }
+    def "should not deactivate Admin Accounts"() {
+        expect:
+        !adminAccountService.deactivateAdminAccounts([-1, 0])
+    }
+
+    //activateAdminAccounts
+    def "should activate Admin Accounts"() {
+        given:
+        def adminAccount = TestDataGenerator.createAdminAccount()
+        adminAccount.setIsActive(false)
+        def adminAccount1 = TestDataGenerator.createAdminAccount()
+        adminAccount1.setIsActive(false)
+
+        adminAccountService.saveAdminAccount(adminAccount)
+        adminAccountService.saveAdminAccount(adminAccount1)
+
+        when:
+        adminAccountService.activateAdminAccounts([adminAccount.getId(), adminAccount1.getId()])
+
+        then:
+        adminAccount.getIsActive()
+        adminAccount1.getIsActive()
+        noExceptionThrown()
+    }
+    def "should not activate Admin Accounts"() {
+        expect:
+        !adminAccountService.activateAdminAccounts([-1, 0])
+    }
+
+    //getAllAdminAccounts
+    def "should return all Admin Accounts"() {
+        given:
+        def accountsList = adminAccountService.getAllAdminAccounts()
+        def oldNumberOfAccounts = accountsList.size()
+        adminAccountService.saveAdminAccount(TestDataGenerator.createAdminAccount())
+        adminAccountService.saveAdminAccount(TestDataGenerator.createAdminAccount())
+
+        when:
+        accountsList = adminAccountService.getAllAdminAccounts()
+
+        then:
+        oldNumberOfAccounts + 2 == accountsList.size()
+        noExceptionThrown()
+    }
 }
--- skisystem/SkiSystemDomain/src/test/java/pl/polsl/pp/skisystemdomain/TestDataGenerator.groovy
+++ skisystem/SkiSystemDomain/src/test/java/pl/polsl/pp/skisystemdomain/TestDataGenerator.groovy
@@ -16,11 +16,18 @@ class TestDataGenerator {
 
     }
 
-    static AdminAccount createIncorrectAdminAccount(){
+    static AdminAccount createAdminAccountWithoutPassword(){
         Fairy f = Fairy.create()
         Person p = f.person()
 
-        new AdminAccount(p.username(),"",true, p.firstName(), p.lastName(),p.email(),p.telephoneNumber())
+        new AdminAccount(p.username(),null,true, p.firstName(), p.lastName(),p.email(),p.telephoneNumber())
+
+    }
+    static AdminAccount createAdminAccountWithoutUsername(){
+        Fairy f = Fairy.create()
+        Person p = f.person()
+
+        new AdminAccount(null,p.password(),true, p.firstName(), p.lastName(),p.email(),p.telephoneNumber())
 
     }
 }
